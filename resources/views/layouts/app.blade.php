ec991483b1ac7b1c16982929664ce3d9e0495cdb
salmanpatnee <salmanpatni92@gmail.com>
2025-07-09 18:36:34 +0500

    Update master layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,41 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
             <!-- ===== Main Content Start ===== -->
             <main>
                 <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
-                    @yield('content')
+                    <!-- Breadcrumb Start -->
+                    <div x-data="{ pageName: `
+                                        Page Title` }">
+                        <div class="mb-6 flex flex-wrap items-center justify-between gap-3">
+                            <h2 class="text-xl font-semibold text-gray-800 dark:text-white/90" x-text="pageName"></h2>
+
+                            <nav>
+                                <ol class="flex items-center gap-1.5">
+                                    <li>
+                                        <a class="inline-flex items-center gap-1.5 text-sm text-gray-500 dark:text-gray-400"
+                                            href="{{ route('compliance') }}">
+                                            Home
+                                            <svg class="stroke-current" width="17" height="16"
+                                                viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M6.0765 12.667L10.2432 8.50033L6.0765 4.33366" stroke=""
+                                                    stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                    <li class="text-sm text-gray-800 dark:text-white/90" x-text="pageName"></li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                    <!-- Breadcrumb End -->
+                    <div
+                        class="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7  dark:border-gray-800 dark:bg-white/[0.03]">
+                        <div class="mx-auto w-full text-center">
+                            <h3 class="text-theme-xl mb-4 font-semibold text-gray-800 sm:text-2xl dark:text-white/90">
+                                Content Goes Here
+                            </h3>
+                            {{-- @yield('content') --}}
+                        </div>
+                    </div>
+
                 </div>
             </main>
             <!-- ===== Main Content End ===== -->
