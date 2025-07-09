33f9cb899a5e3b1a62a42cc1bdc79d3112dd4583
salmanpatnee <salmanpatni92@gmail.com>
2025-07-09 17:57:08 +0500

    Remove unused code

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Option;
 use Carbon\Carbon;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -42,37 +41,6 @@ class LoginController extends Controller
 
         return redirect('/home')->with('success', 'Welcome Back!');
     }
-
-
-    // public function store()
-    // {
-
-    //     $attributes = request()->validate([
-    //         'username'  => 'required|string',
-    //         'password'  => 'required'
-    //     ]);
-
-
-    //     $systemExpiryDate = Option::select('value')->where('key', 'system_expired_at')->first();
-    //     $todaysDate = Carbon::today();
-
-    //     if(Carbon::parse($systemExpiryDate->value)->lt($todaysDate)){
-    //         throw ValidationException::withMessages([
-    //             'username' => 'Your system trail has been expired.'
-    //         ]);
-    //     }
-
-    //     if (!auth()->attempt($attributes)) {
-    //         throw ValidationException::withMessages([
-    //             'username' => 'Your provided credentials could not be verified.'
-    //         ]);
-    //     }
-
-    //     session()->regenerate();
-
-    //     // return redirect()->intended('/')->with('success', 'Welcome Back!');
-    //     return redirect('/home')->with('success', 'Welcome Back!');
-    // }
 
     public function destroy()
     {
