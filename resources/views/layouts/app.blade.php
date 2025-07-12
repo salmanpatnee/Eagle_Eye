45caa447c7681f133383003a5e864c0568825ee2
salmanpatnee <salmanpatni92@gmail.com>
2025-07-12 10:58:58 +0500

    Update master template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,80 +11,14 @@
             <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
                 @include('partials.breadcrumbs')
                 <div
-                    class="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7  dark:border-gray-800 dark:bg-white/[0.03]">
+                    class="min-h-screen rounded-lg border border-gray-200 bg-white p-4  dark:border-gray-800 dark:bg-white/[0.03]">
                     <div class="mx-auto w-full">
+
                         <div
-                            class="overflow-hidden rounded-2xl border border-gray-200 bg-white pt-4 dark:border-gray-800 dark:bg-white/[0.03]">
-                            <div class="flex flex-col gap-5 px-6 mb-4 sm:flex-row sm:items-center sm:justify-end">
-                                <div class="flex items-center gap-5">
-                                    <x-action.button label="View" label_ar="منظر" />
-                                    <x-action.button label="Add" label_ar="يضيف" />
-                                    <x-action.button label="Update" label_ar="تحديث" />
-                                    <x-action.button label="Delete" label_ar="يمسح" />
-
-
-
-
-                                </div>
-                            </div>
-
-                            <div class="max-w-full overflow-x-auto custom-scrollbar">
-                                <table class="min-w-full">
-                                    <!-- table header start -->
-                                    <thead
-                                        class="border-gray-100 border-y bg-gray-50 dark:border-gray-800 dark:bg-gray-900 text-left">
-                                        <tr>
-                                            <x-table.th label="S.No" label_ar="رقم" />
-                                            <x-table.th label="Organization ID" label_ar="رمز للجهة" />
-                                            <x-table.th label="Organization Name" label_ar="الاسم للجهة" />
-                                            <x-table.th label="Contact Number" label_ar="رقم الاتصال" />
-                                            <x-table.th label="Email Address" label_ar="عنوان البريد الإلكتروني" />
-                                        </tr>
-                                    </thead>
-                                    <!-- table header end -->
-
-                                    <!-- table body start -->
-                                    <tbody class="divide-y divide-gray-100 dark:divide-gray-800">
-                                        <tr>
-                                            <td class="px-6 py-3 whitespace-nowrap">
-                                                <span
-                                                    class="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-                                                    1
-                                                </span>
-
-                                            </td>
-                                            <td class="px-6 py-3 whitespace-nowrap">
-                                                <span
-                                                    class="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-                                                    ORG-001
-                                                </span>
-                                            </td>
-                                            <td class="px-6 py-3 whitespace-nowrap">
-                                                <span
-                                                    class="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-                                                    ACME Corporation
-                                                </span>
-                                            </td>
-                                            <td class="px-6 py-3 whitespace-nowrap">
-                                                <span
-                                                    class="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-                                                    00966-55-6612345
-                                                </span>
-                                            </td>
-                                            <td class="px-6 py-3 whitespace-nowrap">
-                                                <span
-                                                    class="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-                                                    acme@abcde.com
-                                                </span>
-                                            </td>
-
-                                        </tr>
-                                    </tbody>
-                                    <!-- table body end -->
-                                </table>
-                            </div>
+                            class="relative max-w-full overflow-x-auto   rounded-lg border border-gray-200 bg-white pt-4 dark:border-gray-800 dark:bg-white/[0.03]">
+                            @yield('content')
                         </div>
-                        {{-- @yield('content') --}}
+
                     </div>
                 </div>
 
