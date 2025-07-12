586ccb30e5eaecbc58c6720918f630f4115bc306
salmanpatnee <salmanpatni92@gmail.com>
2025-07-12 16:54:56 +0500

    Add components

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -2,24 +2,17 @@
     'label' => '',
     'for' => '',
     'required' => false,
-    'rtl' => [],
+    'label_ar' => [],
 ])
 
-<div class="flex items-center justify-between">
-    <label 
-        class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400" 
-        for="{{ $for }}"
-    >
-        {{ $label }}@if(!empty($required) && $required !== 'false')<span class="text-error-500">*</span>@endif
-    </label>
-    @if(isset($rtl['label']))
-        <label 
-            for="{{ $for }}" 
-            dir="rtl" 
-            lang="ar"
-            class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400 text-right"
-        >
-            {{ $rtl['label'] }}
-        </label>
+<div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-1.5">
+    <span class="font-bold sm:order-1 sm:text-right sm:w-auto text-left w-full">
+        {{ $label }}
+    </span>
+    @if (isset($label_ar))
+        <span class="font-bold sm:ml-2 sm:order-2 sm:text-left sm:w-auto text-left text-sm w-full" dir="rtl"
+            lang="ar" aria-label="{{ $label_ar }}">
+            {{ $label_ar }}
+        </span>
     @endif
 </div>
--- resources/views/components/info-col.blade.php
+++ resources/views/components/info-col.blade.php
@@ -5,15 +5,8 @@
 
 
 <div class="border rounded-lg p-3 bg-white">
-    <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-1.5">
-        <span class="font-bold sm:order-1 sm:text-right sm:w-auto text-left w-full">
-            {{ $label }}
-        </span>
-        <span class="font-bold sm:ml-2 sm:order-2 sm:text-left sm:w-auto text-left text-sm w-full" dir="rtl"
-            lang="ar" aria-label="{{ $label_ar }}">
-            {{ $label_ar }}
-        </span>
-    </div>
+    <x-form.label :label="$label" :label_ar="$label_ar" />
+
     <div class="px-0 py-2.5 text-base font-medium text-gray-600  text-left">
         {{ $slot }}
     </div>
