d7280769207db9d65fda142d15a898eb55f224a1
salmanpatnee <salmanpatni92@gmail.com>
2025-07-12 14:15:57 +0500

    Fix alignment of content

--- resources/views/components/action/button.blade.php
+++ resources/views/components/action/button.blade.php
@@ -11,11 +11,8 @@
     <a href="{{ route($route_name) }}" {{ $attributes->merge(['class' => 'inline-block']) }}>
 @endif
 <button type="{{ $type }}"
-    {{ $attributes->merge(['class' => "bg-brand-950 font-medium hover:bg-brand-600 inline-flex items-center rounded-lg shadow-theme-xs text-white transition px-3 py-1.5 text-xs md:px-7 md:py-3 md:text-sm $class"]) }}>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        width="20" height="20">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-    </svg>
+    {{ $attributes->merge(['class' => "bg-brand-950 font-medium hover:bg-brand-600 inline-flex items-center p-3 rounded-lg shadow-theme-xs text-sm text-white transition $class"]) }}>
+
     <span class="inline mx-2">{{ $label }}</span>
     <span class="inline text-xs font-semibold leading-tight " dir="rtl" lang="ar">{{ $label_ar }}</span>
 </button>
--- resources/views/components/table/action-wrapper.blade.php
+++ resources/views/components/table/action-wrapper.blade.php
@@ -1,3 +1,14 @@
-<div class="flex px-3 mb-4 items-center justify-end">
-    {{ $slot }}
+@props([
+    'title' => '',
+])
+
+<div class="flex flex-col items-center sm:flex-row sm:items-center sm:justify-between px-3 mb-4 gap-2">
+    <div class="w-full sm:w-auto flex justify-center sm:justify-start">
+        @if ($title)
+            <h1 class="font-medium text-lg text-center sm:text-left"> {{ $title }}</h1>
+        @endif
+    </div>
+    <div class="w-full sm:w-auto flex justify-center sm:justify-end">
+        {{ $slot }}
+    </div>
 </div>
