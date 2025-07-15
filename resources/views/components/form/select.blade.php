9f4ef0be807999b6afabf4cdbfc381f9cf21bc5f
salmanpatnee <salmanpatni92@gmail.com>
2025-07-15 16:20:23 +0500

    Update domain form

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -23,8 +23,7 @@
         </option>
         @foreach ($data as $row)
             <option value="{{ $row->$id_key }}" class="text-gray-700" @if (old($name, $value) == $row->$id_key) selected @endif>
-                {{ $row->$id_key }} {{ $row->$value_key }}
-            </option>
+                {{ $row->$id_key }} {{ $row->$value_key }}</option>
         @endforeach
     </select>
 
