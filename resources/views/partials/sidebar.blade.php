8e8b6881e34c86f0710bec58074fe916d95758ca
salmanpatnee <salmanpatni92@gmail.com>
2025-07-15 10:26:11 +0500

    Add links

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -34,10 +34,22 @@
                  </h3>
                  <ul class="flex flex-col gap-4 mb-6">
 
-                     <x-sidebar-menu-item route_name="organizations.index" label_ar="إعداد الجهة"
-                         label="Organizations" />
-                     <x-sidebar-menu-item route_name="locations.index" label_ar="إعداد الجهة" label="Locations" />
-
+                     <x-sidebar-menu-item route_name="organizations.index" label_ar="إعداد الجهة"       
+                     label="Organizations" />
+                     <x-sidebar-menu-item route_name="locations.index" label_ar="الموقع" label="Locations" />
+                     <x-sidebar-menu-item route_name="departments.index" label_ar="القسم" label="Department" />
+                     <x-sidebar-menu-item route_name="sub-departments.index" label_ar="القسم الفرعي" label="Sub-Department" />
+                     <x-sidebar-menu-item route_name="classifications.index" label_ar="التصنيف" label="Classification" />
+                     <x-sidebar-menu-item route_name="categories.index" label_ar="الفئة" label="Category" />
+                     <x-sidebar-menu-item route_name="sub-categories.index" label_ar="الفئة الفرعية" label="Sub-Category" />
+                     <x-sidebar-menu-item route_name="best-practices.index" label_ar="أفضل الممارسات" label="Best Practices" />
+                     <x-sidebar-menu-item route_name="domains.index" label_ar="قائمة النطاقات" label="Domain List" />
+                     <x-sidebar-menu-item route_name="sub-domains.index" label_ar="قائمة النطاقات الفرعية" label="Sub-Domain List" />
+                     {{-- <x-sidebar-menu-item route_name="owner-list.index" label_ar="تسجيل المالك" label="Owner Registration" /> --}}
+                     {{-- <x-sidebar-menu-item route_name="owner-role-list.index" label_ar="دور المالك" label="Owner Role" /> --}}
+                     {{-- <x-sidebar-menu-item route_name="custodian-list.index" label_ar="تسجيل المسؤول" label="Custodian Registration" /> --}}
+                     {{-- <x-sidebar-menu-item route_name="custodian-role-list.index" label_ar="دور المسؤول" label="Custodian Role" /> --}}
+                     {{-- <x-sidebar-menu-item route_name="options.index" label_ar="إعدادات" label="Settings" /> --}}
 
 
                      <!-- Menu Item Pages -->
