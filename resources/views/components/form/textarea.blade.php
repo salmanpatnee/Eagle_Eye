f7edf1163cb3956bb6546dfee07ce0ed43308e51
salmanpatnee <salmanpatni92@gmail.com>
2025-08-23 11:25:03 +0500

    Fix textarea attributes in Blade component for proper HTML structure and output handling

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -9,12 +9,8 @@
 ])
 
 
-<textarea id="{{ $name }}" name="{{ $name }}" row="{{ $rows }}"
+<textarea id="{{ $name }}" name="{{ $name }}" rows="{{ $rows }}"
     @if ($required) required @endif placeholder="{{ $placeholder }}"
     {{ $attributes->merge([
         'class' => "bg-transparent border border-gray-300 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 placeholder:text-gray-400 px-4 py-2.5 rounded-lg shadow-theme-xs text-gray-800 text-sm w-full $class",
-    ]) }}>
-@if (!$html)
-{{ old($name, $value) }}@else{!! old($name, $value) !!}
-@endif
-</textarea>
+    ]) }}>{!! $html ? trim(old($name, $value)) : e(trim(old($name, $value))) !!}</textarea>
