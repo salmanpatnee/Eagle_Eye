afcd1900770bfd5f30a46071dc95031b2c5a62be
salmanpatnee <salmanpatni92@gmail.com>
2025-09-19 10:01:38 +0500

    Update checklist label from "Checklist for CISO" to "Checklist for ITSM Manager"

--- resources/views/components/iso-checklist.blade.php
+++ resources/views/components/iso-checklist.blade.php
@@ -13,6 +13,6 @@
             </svg>
         </div>
         <p class="text-gray-700 font-medium group-hover:text-brand-700 transition-colors duration-300">
-            Checklist for CISO</p>
+            Checklist for ITSM Manager</p>
     </div>
 </a>
