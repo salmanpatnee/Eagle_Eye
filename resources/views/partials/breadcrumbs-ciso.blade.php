1669208734807427eb200e3b9760170377821384
salmanpatnee <salmanpatni92@gmail.com>
2025-12-17 11:54:16 +0500

    Update the product pages breadcrumb

--- resources/views/partials/breadcrumbs-ciso.blade.php
+++ resources/views/partials/breadcrumbs-ciso.blade.php
@@ -25,7 +25,7 @@
                         </svg>
                     </a>
                 </li>
-                @if(request()->route()->getName() == 'process.view.show')
+                @if(request()->route()->getName() == 'process.view.show' || request()->route()->getName() == 'process.resource.checklist' || request()->route()->getName() == 'process.resource.videos' || request()->route()->getName() == 'process.resource.glossary' || request()->route()->getName() == 'process.resource.template')
                 <li>
                     <a class="inline-flex items-center gap-1.5 text-sm text-gray-500 dark:text-gray-400"
                         href="{{ route('ciso-process.index') }}">
@@ -38,6 +38,21 @@
                     </a>
                 </li>
                 @endif
+
+                @if(str_starts_with(request()->route()->getName(), 'products.'))
+                <li>
+                    <a class="inline-flex items-center gap-1.5 text-sm text-gray-500 dark:text-gray-400"
+                        href="{{ route('ciso-products.index') }}">
+                        Products
+                        <svg class="stroke-current" width="17" height="16" viewBox="0 0 17 16" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.0765 12.667L10.2432 8.50033L6.0765 4.33366" stroke="" stroke-width="1.2"
+                                stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </a>
+                </li>
+                @endif
+
 
                 <li class="text-sm text-gray-800 dark:text-white/90">
                     @yield('title')
